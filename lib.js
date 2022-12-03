e7c9dbdd8df7396a6a682329aa86d1c5cc27be8a
John Dahl <john.dahl@ajuvo.com>
2022-12-03 10:30:32 -0600

    Added comments to the array methods

--- lib.js
+++ lib.js
@@ -11,6 +11,11 @@ if( !Array.prototype.newMethod ){
 */
 
 exports.readFile = function( filename, cleanFunction ){
+    /* Given a file name (including path if required) and
+     * an optional function to clean or manipulate the content,
+     * retrieve the file's contents, apply the clean function, and
+     * return the resulting data.
+     */
     var data = fs.readFileSync(filename, 'utf-8');
     if( cleanFunction !== undefined ){
         return cleanFunction( data );
@@ -22,9 +27,12 @@ exports.readFile = function( filename, cleanFunction ){
 
 if( !Array.prototype.sum ){
     Array.prototype.sum = function(){
-
+        /* Given an array of numeric values,
+         * add the elements together and
+         * return the result.
+         */
         return this.reduce( function( total, value ){
-            return total += value;
+            return total += Number( value );
         }, 0 );
     }
 }
@@ -32,27 +40,43 @@ if( !Array.prototype.sum ){
 
 if( !Array.prototype.product ){
     Array.prototype.product = function(){
-        
+        /* Given an array of numeric values,
+         * multiple the values together and
+         * return the result.
+         */
+
         return this.reduce( function( total, value ){
-            return total *= value;
+            return total *= Number( value );
         }, 1 );
     }
 }
 
 if( !Array.prototype.parseInt ){
     Array.prototype.parseInt = function(){
+        /* Given an array of values,
+         * parse each value into an Integer.
+         * return a new array.
+         */
         return this.map( num => parseInt( num ) );
     }
 }
 
 if( !Array.prototype.parseNum ){
     Array.prototype.parseNum = function(){
+        /* Given an array of values,
+         * convert each value into a number.
+         * return a new array.
+         */
         return this.map( num => Number( num ) );
     }
 }
 
 if( !Array.prototype.numSort ){
     Array.prototype.numSort = function( order ){
+        /* Given an array of values and optionally an order value,
+         * perform a numeric sort of the values in the order provided or ascending.
+         * return a new array.
+         */
         ( order == 'desc' ) ? this.sort( ( a, b ) => b-a ) : this.sort( ( a, b ) => a-b );
         return this;
     }
@@ -60,12 +84,20 @@ if( !Array.prototype.numSort ){
 
 if( !Array.prototype.intersection ){
     Array.prototype.intersection = function( arr ){
+        /* Given an array of values and a second array as a parameter,
+         * identify the values that appear in both arrays.
+         * return a new array.
+         */
         return this.filter( x => arr.indexOf( x ) > -1 );
     }
 }
 
 if( !Array.prototype.union ){
     Array.prototype.union = function( arr ){
+        /* Given an array of values and a second array as a parameter,
+         * identify all of the values that appear in either array.
+         * Remove any duplicates and return a new array.
+         */
         return this
         .concat( arr )
         .deduplicate();
@@ -74,6 +106,11 @@ if( !Array.prototype.union ){
 
 if( !Array.prototype.deduplicate ){
     Array.prototype.deduplicate = function(){
+        /* Given an array of values,
+         * identify the values that appear more than once.
+         * Retain the first occurrence and remove the rest.
+         * return a new array.
+         */
         return this.filter( ( x, idx, arr ) => arr.indexOf( x ) == idx );
     }
 }
