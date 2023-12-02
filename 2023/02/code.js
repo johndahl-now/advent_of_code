3cdf5ce92e15e54ba1d01f21a563f5ca51c8cd5e
John <johndahl.now@gmail.com>
2023-12-01 23:32:33 -0600

    Completed day 2

--- 2023/02/code.js
+++ 2023/02/code.js
@@ -15,12 +15,12 @@ var lib = require( '../../lib' ),
     "Part 1": {
         'Sample Expected': 'N/A',
         'Sample Calculated': part1( sample ), 
-        // 'Real Calculated': part1( input ) 
+        'Real Calculated': part1( input ) 
     },
     "Part 2": {
         'Sample Expected': 'N/A',
         'Sample Calculated': part2( sample ), 
-        // 'Real Calculated': part2( input ) 
+        'Real Calculated': part2( input ) 
     }
 };
 
@@ -37,10 +37,71 @@ function cleanData( data ){
 }
 
 function part1( data ){
-    return data;
+    var rgb = [ 12, 13, 14 ];
+    var possible_games = [];
+
+    data.forEach( ( game ) => {
+        var game_id = game.match( /Game (\d+):/ )[1];
+
+        var max_rgb = {
+            'red': 0,
+            'green': 0,
+            'blue': 0
+        };
+
+        var hands = game.match( /Game \d+: (.*)/ )[1].split( '; ' );
+        
+        hands.forEach( ( hand ) => {
+            hand = hand.split( ', ' );
+            
+            hand.forEach( ( color ) => {
+                color = color.split(' ');
+                max_rgb[ color[1] ] = Math.max( max_rgb[ color[1] ], color[0] );
+            } );
+
+        } );
+
+        var possible = max_rgb.red <= rgb[0] && max_rgb.green <= rgb[1] && max_rgb.blue <= rgb[2];
+        if( possible ){
+            possible_games.push( game_id );
+        }
+    } )
+
+    return possible_games.sum();
+
 }
 
 function part2( data ){
-    return data;
+    var rgb = [ 12, 13, 14 ];
+    var game_powers = [];
+
+    data.forEach( ( game ) => {
+        var power;
+
+        var min_rgb = {
+            'red': 0,
+            'green': 0,
+            'blue': 0
+        };
+
+        var hands = game.match( /Game \d+: (.*)/ )[1].split( '; ' );
+        
+        hands.forEach( ( hand ) => {
+            hand = hand.split( ', ' );
+            
+            hand.forEach( ( color ) => {
+                color = color.split(' ');
+                min_rgb[ color[1] ] = Math.max( min_rgb[ color[1] ], color[0] );
+            } );
+
+        } );
+
+        power = min_rgb.red * min_rgb.green * min_rgb.blue;
+
+        game_powers.push( power );
+        
+    } )
+
+    return game_powers.sum();
 }
 
