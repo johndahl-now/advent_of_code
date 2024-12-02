f4f0a9fdb9833133e3eb807cde46eb946c028981
John Dahl <johndahl.now@gmail.com>
2024-12-02 13:32:18 -0600

    Completed day 2

--- 2024/day_02/code.js
+++ 2024/day_02/code.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 var lib = require( '../../lib' ),
-    sample = lib.readFile( 'input_sample.txt', cleanData ),
+    sample = lib.readFile( 'input_sample.txt', cleanData );
     input = lib.readFile( 'input.txt', cleanData );
     
 
@@ -11,19 +11,25 @@ var lib = require( '../../lib' ),
 
  var result = {
     "Part 1": {
-        'Sample Expected': 'N/A',
-        'Sample Calculated': part1( sample ), 
-        // 'Real Calculated': part1( input ) 
+        'Sample Expected': 2,
+        'Sample Calculated': part1( sample.slice(0) ), 
+        'Real Calculated': part1( input.slice(0) ) 
     },
     "Part 2": {
-        'Sample Expected': 'N/A',
-        'Sample Calculated': part2( sample ), 
-        // 'Real Calculated': part2( input ) 
+        'Sample Expected': 4,
+        'Sample Calculated': part2( sample.slice(0) ), 
+        'Real Calculated': part2( input.slice(0) ) 
     }
 };
 
 console.table( result );
 /* Result:
+┌─────────┬─────────────────┬───────────────────┬─────────────────┐
+│ (index) │ Sample Expected │ Sample Calculated │ Real Calculated │
+├─────────┼─────────────────┼───────────────────┼─────────────────┤
+│ Part 1  │ 2               │ 2                 │ 670             │
+│ Part 2  │ 4               │ 4                 │ 700             │
+└─────────┴─────────────────┴───────────────────┴─────────────────┘
 */
 
 /*****************************************
@@ -31,14 +37,120 @@ console.table( result );
  *****************************************/ 
 
 function cleanData( data ){
-    return data.split('\n');
+   /* Given a string containing a grid of data representing:
+    * reports (rows) and levels (columns),
+    * convert the string into a matrix (array of arrays),
+    * Return the matrix.
+    */
+
+   return data
+    .split('\n')
+    .map( row => row.split( " " ) )
+    .map( row => row.parseInt() );
+
 }
 
 function part1( data ){
-    return data;
+    /* Given a matrix (array of arrays) of values,
+     * examine each row and determine if:
+     *    1. the numbers are sorted in either direction and
+     *    2. the difference between any two values is between 1 and 3 inclusively.
+     * Return the number of safe reports.
+     */
+    var safe_reports = 0;
+
+    // Filter out reports that are not fully ascending or descending.
+    data = data.filter( report => {
+        
+        // Make sorted copies of the report.
+        var sortedAsc = report.slice(0).numSort(),
+            sortedDesc = report.slice(0).numSort( 'desc' );
+
+        // Convert reports to strings
+        sortedAsc = sortedAsc.join(',');
+        sortedDesc = sortedDesc.join(',');
+        report = report.join(',');
+
+        return ( report === sortedAsc ) || ( report === sortedDesc );
+
+    } );
+
+    // Filter out reports with a variance less than 1 or greater than 3
+    data = data.filter( report => {
+        var variance;
+
+        for( var i=1; i<report.length; i++ ){
+        
+            variance = Math.abs( report[ i ] - report[ i-1 ] );
+        
+            if( variance == 0 || variance > 3 ) return false;
+        
+        }
+
+        return true;
+    } );
+
+    return data.length;
+
 }
 
 function part2( data ){
-    return data;
+    /* Given a matrix (array of arrays) of values,
+     * examine each row and determine if:
+     *    1. the numbers are sorted in either direction and
+     *    2. the difference between any two values is between 1 and 3 inclusively.
+     * If a level fails a test, see if removing a single level will render the report safe.
+     * Return the number of safe reports.
+     */
+
+    data = data.filter( report => {
+
+        var safeReport = isReportSafe( report );
+        if( safeReport ) return true;
+
+        // Determine if removing a single level will make the report safe.
+        var reducedReport;
+        for( var i=0; i<report.length; i++ ){
+            reducedReport = removeLevel( report, i );
+            if( isReportSafe( reducedReport ) ) return true;
+        }
+
+        // The report is not safe, either unmodified or by removing a single level.
+        return false;
+
+    } );
+
+    return data.length;
 }
 
+function isReportSafe( report ){
+    var variance
+
+    // Make sorted copies of the report.
+    var sortedAsc = report.slice(0).numSort(),
+        sortedDesc = report.slice(0).numSort( 'desc' );
+
+    // Convert reports to strings
+    sortedAsc = sortedAsc.join(',');
+    sortedDesc = sortedDesc.join(',');
+
+    if ( report.join(',') !== sortedAsc  &&  report.join(',') !== sortedDesc ) return false;
+
+    for( var i=1; i<report.length; i++ ){
+
+        variance = Math.abs( report[ i ] - report[ i-1 ] );
+
+        if( variance == 0 || variance > 3 ) return false;
+
+    }
+
+    return true;
+}
+
+function removeLevel( report, index ){
+    /* Given a report and an index,
+     * remove the level at the given index.
+     * Return a NEW report without the indexed level.
+     */
+    return report.slice( 0, index ).concat( report.slice( index + 1 ) );
+}
